c833d987af63469f45216da5dacd7553eb4683aa
widyacitrayu <widyac431@gmail.com>
2025-06-10 13:17:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Selamat Datang Di Web Widya")
+st.write( "Mari bermain bersama widya
+         "
+        ) 
